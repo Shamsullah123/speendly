484c351f44671512dd126798dec24bb6f9e75bb1
Shamsullah123 <shamsullahkhan94@gmail.com>
2026-08-07 06:57:28 +0500

    database setup

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
 
+from database.db import get_db, init_db, seed_db
+
 app = Flask(__name__)
+
+# Make sure the schema and demo data exist before any request is served.
+with app.app_context():
+    init_db()
+    seed_db()
 
 
 # ------------------------------------------------------------------ #
